cd4998822e489a4b362f2c527ff79d467df7aa9a
mlm09kdev <mlm09kdev@gmail.com>
2019-05-16 15:57:00 -0400

    Fixed the Save/Load bug that prevent the level and score to be saved over app restart and implemented Runtime Permissions.

--- app/src/main/java/com/mlm09kdev/jump_jump/JumpJumpActivity.java
+++ app/src/main/java/com/mlm09kdev/jump_jump/JumpJumpActivity.java
@@ -1,11 +1,19 @@
 package com.mlm09kdev.jump_jump;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Toast;
 
 import com.mlm09kdev.jump_jump.Framework.Impl.GLGame;
 import com.mlm09kdev.jump_jump.Framework.Screen;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
@@ -14,16 +22,23 @@ import javax.microedition.khronos.opengles.GL10;
  * Created by Manuel Montes de Oca on 5/4/2019.
  */
 public class JumpJumpActivity extends GLGame {
+    private static final int PERMISSIONS_REQUEST_CODE = 1;
+    String[] appPermission = {Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.ACCESS_FINE_LOCATION};
     Boolean firstTimeCreated = true;
 
     @Override
     public Screen getStartScreen() {
-        return new MainMenuScreen(this);
+        if (checkAndRequestPermissions())
+        {
+        //Do something here if they accept
+        }
+            return new MainMenuScreen(this);
     }
 
     @Override
     public void onSurfaceCreated(GL10 gl, EGLConfig config) {
         super.onSurfaceCreated(gl, config);
+
         if (firstTimeCreated) {
             Settings.load(getFileIO());
             Assets.load(this);
@@ -42,12 +57,51 @@ public class JumpJumpActivity extends GLGame {
     public void onPause() {
         super.onPause();
         if (Settings.soundEnabled) {
-        Assets.music.pause();
+            Assets.music.pause();
         }
     }
 
     @Override
     public boolean onTouch(View v, MotionEvent event) {
         return false;
+    }
+
+    private boolean checkAndRequestPermissions() {
+        List<String> listPermissionsNeeded = new ArrayList<>();
+        for (String permission : appPermission) {
+            if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+                listPermissionsNeeded.add(permission);
+            }
+        }
+        if (!listPermissionsNeeded.isEmpty()) {
+            ActivityCompat.requestPermissions(this,
+                    listPermissionsNeeded.toArray((new String[listPermissionsNeeded.size()]))
+                    ,PERMISSIONS_REQUEST_CODE);
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    Toast.makeText(this, "Permission denied to read your External storage, save functionality disabled", Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
     }
 }
--- app/src/main/java/com/mlm09kdev/jump_jump/Settings.java
+++ app/src/main/java/com/mlm09kdev/jump_jump/Settings.java
@@ -1,6 +1,7 @@
 package com.mlm09kdev.jump_jump;
 
 import com.mlm09kdev.jump_jump.Framework.FileIO;
+import com.mlm09kdev.jump_jump.GameObjects.World;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -16,53 +17,64 @@ public class Settings {
     public final static int[] highscores = new int[] { 100, 80, 50, 30, 10 };
     public final static String file = ".jump_jump_settings.txt";
 
-    public static void load (FileIO files){
+    public static void load(FileIO files) {
         BufferedReader in = null;
 
         try {
             in = new BufferedReader(new InputStreamReader(files.readFile(file)));
             soundEnabled = Boolean.parseBoolean(in.readLine());
+            World.level = Integer.parseInt(in.readLine());
+            World.score = Integer.parseInt(in.readLine());
             for (int i = 0; i < 5; i++) {
                 highscores[i] = Integer.parseInt(in.readLine());
             }
         } catch (IOException e) {
-
+            e.printStackTrace();
         } catch (NumberFormatException e) {
+            e.printStackTrace();
         } finally {
             try {
                 if (in != null) {
                     in.close();
                 }
             } catch (IOException e) {
+                e.printStackTrace();
             }
         }
     }
-    public static void save (FileIO files) {
+    public static void save(FileIO files) {
         BufferedWriter out = null;
         try {
             out = new BufferedWriter(new OutputStreamWriter(
                     files.writeFile(file)));
             out.write(Boolean.toString(soundEnabled));
             out.write("\n");
-            for(int i = 0; i < 5; i++) {
+            out.write(Integer.toString(World.level));
+            out.write("\n");
+            out.write(Integer.toString(World.score));
+            out.write("\n");
+            for (int i = 0; i < 5; i++) {
                 out.write(Integer.toString(highscores[i]));
                 out.write("\n");
             }
 
         } catch (IOException e) {
+            e.printStackTrace();
         } finally {
             try {
                 if (out != null)
                     out.close();
             } catch (IOException e) {
+                e.printStackTrace();
             }
         }
     }
+
     public static void addScore(int score) {
-        for(int i=0; i < 5; i++) {
-            if(highscores[i] < score) {
-                for(int j= 4; j > i; j--)
-                    highscores[j] = highscores[j-1];
+        for (int i = 0; i < 5; i++) {
+            if (highscores[i] < score) {
+                for (int j = 4; j > i; j--)
+                    highscores[j] = highscores[j - 1];
                 highscores[i] = score;
                 break;
             }
